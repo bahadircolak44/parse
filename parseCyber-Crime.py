a4f7538ee78e8761d070ad5c01eeed64f73d95e3
bahadircolak44 <bahdrcolak@gmail.com>
2017-10-19 20:51:55 +0300

    Split and parse the domains

--- parseCyber-Crime.py
+++ parseCyber-Crime.py
@@ -1,11 +1,21 @@
 import urllib2
 from BeautifulSoup import BeautifulSoup
 
-url = "http://cybercrime-tracker.net/index.php"
-soup = BeautifulSoup(urllib2.urlopen(url).read())
-x = soup.findAll('tbody')
-for row in x:
-    y = soup.findAll('tr')
-    for td in y:
-        first_column = td.findAll('td')[1]
-        print first_column.text
+temp=0
+count=0
+total=""
+while(count<160):
+    url = "http://cybercrime-tracker.net/index.php?s="+str(count)+"&m=40"
+    temp=temp+1
+    print "\n"+str(temp)+". Page:"
+    count=count+40
+    soup = BeautifulSoup(urllib2.urlopen(url).read())
+    x = soup.findAll('tbody')
+    for row in x:
+        y = soup.findAll('tr')
+        for td in y:
+            first_column = td.findAll('td')[1]
+            a=first_column.text.split("/",1)[0]
+            print a.replace("-::URL","")
+            total+= a + "\n"
+#print total
