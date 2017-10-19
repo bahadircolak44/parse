9f2b46f99e4e01b876a7372683ac5d5cf48b102d
bahadircolak44 <bahdrcolak@gmail.com>
2017-10-19 23:45:28 +0300

    The Last 10 Days

--- parseCyber-Crime.py
+++ parseCyber-Crime.py
@@ -1,21 +1,33 @@
 import urllib2
 from BeautifulSoup import BeautifulSoup
+from datetime import date
+from datetime import timedelta
 
-temp=0
-count=0
-total=""
-while(count<160):
-    url = "http://cybercrime-tracker.net/index.php?s="+str(count)+"&m=40"
-    temp=temp+1
-    print "\n"+str(temp)+". Page:"
-    count=count+40
+
+control = True
+while control:
+    print " The Last 10 Days: "
+    url = "http://cybercrime-tracker.net/index.php"
     soup = BeautifulSoup(urllib2.urlopen(url).read())
+    
     x = soup.findAll('tbody')
     for row in x:
         y = soup.findAll('tr')
         for td in y:
-            first_column = td.findAll('td')[1]
-            a=first_column.text.split("/",1)[0]
-            print a.replace("-::URL","")
-            total+= a + "\n"
-#print total
+            dates = td.findAll('td')[0]
+            date_splitted  = dates.text.split("-")
+            
+            domain_column = td.findAll('td')[1]
+            domain=domain_column.text.split("/",1)[0]
+            
+            if len(date_splitted)>2: 
+                time = date(int(date_splitted[2]), int(date_splitted[1]), int(date_splitted[0])) 
+                today = date.today()
+                
+                if time > (today - timedelta(days=10)) :
+                    print dates.text +"\t|\t"+domain.replace("www.","")+"\n",
+                else:
+                    control=False
+                    break
+                
+                
